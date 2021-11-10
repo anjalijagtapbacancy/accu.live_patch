8818b35ebcce4d277d3685019d06cfc720389c04
Abhishek <brinda.rathod@bacancy.com>
2021-11-10 19:34:34 +0530

    fix graph issues, set graph range, add state management

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:csv/csv.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bluetooth_connection/provider_ecg_data.dart';
 import 'package:path_provider/path_provider.dart';
@@ -42,8 +43,6 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
   final FlutterBlue flutterBlue = FlutterBlue.instance;
-  final List<BluetoothDevice> devicesList = [];
-  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -51,10 +50,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _writeController = TextEditingController();
-  BluetoothDevice? _connectedDevice;
   List<BluetoothService>? _services;
   var isServiceStarted = false;
-  BluetoothCharacteristic? readCharacteristic;
   // writeCharacteristic
   var sub;
   List<Color> gradientColors = [
@@ -65,52 +62,54 @@ class _MyHomePageState extends State<MyHomePage> {
   List<double> decimalList = [];
 
   List<FlSpot> spotsListData = [];
+  ProviderEcgData? providerEcgDataRead;
+  ProviderEcgData? providerEcgDataWatch;
 
   _addDeviceTolist(final BluetoothDevice device) {
-    if (!widget.devicesList.contains(device)) {
-      setState(() {
-        widget.devicesList.add(device);
-      });
-    }
+    providerEcgDataWatch!.setDeviceList(device);
   }
 
   @override
   void initState() {
     super.initState();
 
-    widget.flutterBlue.isOn.then((value) {
-      print("Mirinda isOn ${value.toString()}");
-      if (value) {
-      } else {}
+    SchedulerBinding.instance!.addPostFrameCallback((_) {
+      providerEcgDataRead = context.read<ProviderEcgData>();
+
+      widget.flutterBlue.isOn.then((value) {
+        print("Mirinda isOn ${value.toString()}");
+        if (value) {
+        } else {}
+      });
+
+      widget.flutterBlue.isAvailable.then((value) {
+        print("Mirinda isAvailable ${value.toString()}");
+      });
+
+      widget.flutterBlue.connectedDevices.asStream().listen((List<BluetoothDevice> devices) {
+        print("Mirinda devices ${devices.length}");
+
+        for (BluetoothDevice device in devices) {
+          print("Mirinda connectedDevices ${device}");
+
+          _addDeviceTolist(device);
+        }
+      });
+      widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+        print("Mirinda scan result length devices ${results.length}");
+
+        for (ScanResult result in results) {
+          print("Mirinda scanResults ${result.device}");
+          _addDeviceTolist(result.device);
+        }
+      });
+      widget.flutterBlue.startScan();
     });
-
-    widget.flutterBlue.isAvailable.then((value) {
-      print("Mirinda isAvailable ${value.toString()}");
-    });
-
-    widget.flutterBlue.connectedDevices.asStream().listen((List<BluetoothDevice> devices) {
-      print("Mirinda devices ${devices.length}");
-
-      for (BluetoothDevice device in devices) {
-        print("Mirinda connectedDevices ${device}");
-
-        _addDeviceTolist(device);
-      }
-    });
-    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
-      print("Mirinda scan result length devices ${results.length}");
-
-      for (ScanResult result in results) {
-        print("Mirinda scanResults ${result.device}");
-        _addDeviceTolist(result.device);
-      }
-    });
-    widget.flutterBlue.startScan();
   }
 
   ListView _buildListViewOfDevices() {
     List<Container> containers = [];
-    for (BluetoothDevice device in widget.devicesList) {
+    for (BluetoothDevice device in providerEcgDataWatch!.devicesList) {
       containers.add(
         Container(
           height: 50,
@@ -134,6 +133,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   widget.flutterBlue.stopScan();
                   try {
                     await device.connect();
+                    await device.requestMtu(512);
                   } catch (e) {
                     // if (e.code != 'already_connected') {
                     //   throw e;
@@ -142,9 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   } finally {
                     _services = await device.discoverServices();
                   }
-                  setState(() {
-                    _connectedDevice = device;
-                  });
+                  providerEcgDataWatch!.setConnectedDevice(device);
                 },
               ),
             ],
@@ -165,9 +163,9 @@ class _MyHomePageState extends State<MyHomePage> {
     for (BluetoothService service in _services!) {
       for (BluetoothCharacteristic characteristic in service.characteristics) {
         if (characteristic.uuid.toString() == "0000abf4-0000-1000-8000-00805f9b34fb") {
-          readCharacteristic = characteristic;
+          providerEcgDataWatch!.setReadCharacteristic(characteristic);
           try {
-            generateGraphValuesList(widget.readValues[readCharacteristic!.uuid]);
+            generateGraphValuesList(providerEcgDataWatch!.readValues[characteristic.uuid]);
           } catch (err) {
             print(" caught err ${err.toString()}");
           }
@@ -178,10 +176,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return ListView(
       padding: const EdgeInsets.all(8),
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('Value: ' + decimalList.toString()),
-        ),
         // ...containers,
         Visibility(
             visible: decimalList.isNotEmpty,
@@ -201,6 +195,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             )),
+        // Padding(
+        //   padding: const EdgeInsets.all(8.0),
+        //   child: Text('Value: ' + decimalList.toString()),
+        // ),
       ],
     );
   }
@@ -231,7 +229,8 @@ class _MyHomePageState extends State<MyHomePage> {
         bottomTitles: SideTitles(
           showTitles: true,
           // reservedSize: 22,
-          interval: decimalList.length / (decimalList.length),
+          // interval: decimalList.isNotEmpty ? decimalList.length / 100 : 100,
+          interval: 100,
           getTextStyles: (context, value) =>
               const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 13),
           getTitles: (value) {
@@ -242,10 +241,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // graph data
         leftTitles: SideTitles(
           showTitles: true,
-          interval: (decimalList.isNotEmpty && decimalList.reduce(max) != 0)
-              ? decimalList.reduce(max) / (decimalList.length)
-              : 1,
-          // interval: 100,
+          // interval: (decimalList.isNotEmpty && decimalList.reduce(max) != 0)
+          //     ? decimalList.reduce(max) / (decimalList.length)
+          //     : 1,
+          interval: 2000,
           getTextStyles: (context, value) => const TextStyle(
             color: Color(0xff67727d),
             fontWeight: FontWeight.bold,
@@ -261,10 +260,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       borderData: FlBorderData(show: true, border: Border.all(color: const Color(0xff37434d), width: 1)),
-      minX: 0,
-      maxX: double.parse(decimalList.length.toString()),
-      minY: decimalList.isNotEmpty ? decimalList.reduce(min) : 0,
-      maxY: decimalList.isNotEmpty ? decimalList.reduce(max) : 0,
+      minX: spotsListData.isNotEmpty ? spotsListData.first.x : 0,
+      maxX: spotsListData.isNotEmpty ? spotsListData.last.x : 0,
+
+      // maxX: double.parse(spotsListData.length.toString()),
+      // minY: decimalList.isNotEmpty ? decimalList.reduce(min) : 0,
+      // maxY: decimalList.isNotEmpty ? decimalList.reduce(max) : 0,
+      minY: 0,
+      maxY: 18000,
       lineBarsData: [
         LineChartBarData(
           spots: spotsListData,
@@ -285,89 +288,93 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   ListView _buildView() {
-    if (_connectedDevice != null) {
+    if (providerEcgDataWatch!.connectedDevice != null) {
       return _buildConnectDeviceView();
     }
     return _buildListViewOfDevices();
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          Visibility(
-            visible: decimalList.isNotEmpty,
-            child: TextButton(
-                child: Text(
-                  "Export",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {
-                  _generateCsvFile();
-                }),
-          ),
-          Visibility(
-            visible: _connectedDevice != null,
-            child: TextButton(
-                onPressed: () async {
-                  if (isServiceStarted) {
-                    print("stop service");
-                    isServiceStarted = false;
+  Widget build(BuildContext context) {
+    providerEcgDataWatch = context.watch<ProviderEcgData>();
 
-                    //stop service
-                    //    writeCharacteristic!.write( utf8.encode("0"));
-                    //     if(sub!=null){
-                    //    sub.cancel();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            Visibility(
+              // visible: decimalList.isNotEmpty,
+              visible: false,
+              child: TextButton(
+                  child: Text(
+                    "Export",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    _generateCsvFile();
+                  }),
+            ),
+            Visibility(
+              visible: providerEcgDataWatch!.connectedDevice != null,
+              child: TextButton(
+                  onPressed: () async {
+                    if (isServiceStarted) {
+                      print("stop service");
+                      isServiceStarted = false;
 
-                    //  }
+                      //stop service
+                      //    writeCharacteristic!.write( utf8.encode("0"));
+                      //     if(sub!=null){
+                      //    sub.cancel();
 
-                    await readCharacteristic!.setNotifyValue(false);
-                  } else {
-                    // start service
-                    //  writeCharacteristic!.write(utf8.encode("1"));
-                    print("start service");
-                    // ignore: cancel_subscriptions
-                    if (sub != null) {
-                      sub.cancel();
-                    }
+                      //  }
 
-                    isServiceStarted = true;
+                      await providerEcgDataWatch!.readCharacteristic!.setNotifyValue(false);
+                    } else {
+                      // start service
+                      //  writeCharacteristic!.write(utf8.encode("1"));
+                      print("start service");
+                      // ignore: cancel_subscriptions
+                      if (sub != null) {
+                        sub.cancel();
+                      }
 
-                    await readCharacteristic!.setNotifyValue(true);
-                    await Future.delayed(Duration(seconds: 2));
+                      isServiceStarted = true;
 
-                    sub = readCharacteristic!.value.listen((value) {
-                      setState(() {
-                        widget.readValues[readCharacteristic!.uuid] = value;
+                      await providerEcgDataWatch!.readCharacteristic!.setNotifyValue(true);
+                      await Future.delayed(Duration(seconds: 2));
+
+                      sub = providerEcgDataWatch!.readCharacteristic!.value.listen((value) {
+                        providerEcgDataWatch!.setReadValues(value);
                       });
-                    });
 
-                    await readCharacteristic!.read();
-                  }
-                },
-                child: Text(
-                  isServiceStarted ? "Stop" : "Start",
-                  style: TextStyle(color: Colors.white),
-                )),
-          )
-        ],
-      ),
-      body: _buildView());
+                      await providerEcgDataWatch!.readCharacteristic!.read();
+                    }
+                  },
+                  child: Text(
+                    isServiceStarted ? "Stop" : "Start",
+                    style: TextStyle(color: Colors.white),
+                  )),
+            )
+          ],
+        ),
+        body: _buildView());
+  }
 
   void generateGraphValuesList(List<int>? valueList) {
     if (valueList != null) {
       print("VVV valueList ${valueList.toString()}");
-
-      hexList.clear();
+      if (hexList.length >= 500) {
+        hexList.clear();
+      }
       for (int i = 0; i < valueList.length; i++) {
-        hexList.add(valueList[i].toRadixString(16));
+        hexList.add(valueList[i].toRadixString(16).padLeft(2, '0'));
       }
       print("VVV hexList ${hexList.toString()}");
 
-      decimalList.clear();
-      spotsListData.clear();
-
+      if (decimalList.length >= 500) {
+        decimalList.clear();
+      }
       for (int h = 0; h < hexList.length; h++) {
         if (h % 2 == 0) {
           String strHex = hexList[h + 1] + hexList[h];
@@ -375,12 +382,16 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }
 
-      for (int k = 0; k < hexList.length; k++) {
-        spotsListData.add(FlSpot(double.tryParse(k.toString()) ?? 0, decimalList[k]));
+      spotsListData.clear();
+      for (int k = 0; k < decimalList.length; k++) {
+        // for (int k = decimalList.length; k > 0; k--) {
+        //   if (k == decimalList.length - 5) {
+        //     break;
+        //   }
+        spotsListData.add(FlSpot(double.tryParse((k).toString()) ?? 0, decimalList[k]));
       }
-
       print("VVV decimalList ${decimalList.toString()}");
-      print("VVV spotsListData: ${spotsListData.toList()}");
+      print("VVV length: ${spotsListData.length} spotsListData: ${spotsListData.toList()}");
     }
   }
 
@@ -388,13 +399,6 @@ class _MyHomePageState extends State<MyHomePage> {
     Map<Permission, PermissionStatus> statuses = await [
       Permission.storage,
     ].request();
-
-    List<dynamic> associateList = [
-      {"number": 1, "lat": "14.97534313396318", "lon": "101.22998536005622"},
-      {"number": 2, "lat": "14.97534313396318", "lon": "101.22998536005622"},
-      {"number": 3, "lat": "14.97534313396318", "lon": "101.22998536005622"},
-      {"number": 4, "lat": "14.97534313396318", "lon": "101.22998536005622"}
-    ];
 
     List<List<dynamic>> rows = [];
 
--- lib/provider_ecg_data.dart
+++ lib/provider_ecg_data.dart
@@ -1,3 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
-class ProviderEcgData with ChangeNotifier {}
+class ProviderEcgData with ChangeNotifier {
+  final List<BluetoothDevice> devicesList = [];
+  BluetoothDevice? connectedDevice;
+  bool isLoading = false;
+  BluetoothCharacteristic? readCharacteristic;
+  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
+
+  setLoading(bool value) {
+    isLoading = value;
+    notifyListeners();
+  }
+
+  setDeviceList(BluetoothDevice device) {
+    if (!devicesList.contains(device)) {
+      devicesList.add(device);
+    }
+    notifyListeners();
+  }
+
+  setConnectedDevice(BluetoothDevice device) {
+    connectedDevice = device;
+    notifyListeners();
+  }
+
+  setReadCharacteristic(BluetoothCharacteristic characteristic) {
+    readCharacteristic = characteristic;
+    notifyListeners();
+  }
+
+  setReadValues(List<int> value) {
+    readValues[readCharacteristic!.uuid] = value;
+    notifyListeners();
+  }
+}
